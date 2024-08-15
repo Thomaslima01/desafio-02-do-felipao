764a0303dd40ee33316f41b745b73464c16687ed
Thomaslima01 <thomaslima29@gmail.com>
2024-08-15 18:44:16 -0300

    desafio 03 do felipao

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-function calcularNivel(vitorias, derrotas) {
-    let saldoVitorias = vitorias - derrotas;
-    let nivel;
-
-    if (vitorias < 10) {
-        nivel = 'Ferro';
-    } else if (vitorias >= 11 && vitorias <= 20) {
-        nivel = 'Bronze';
-    } else if (vitorias >= 21 && vitorias <= 50) {
-        nivel = 'Prata';
-    } else if (vitorias >= 51 && vitorias <= 80) {
-        nivel = 'Ouro';
-    } else if (vitorias >= 81 && vitorias <= 90) {
-        nivel = 'Diamante';
-    } else if (vitorias >= 91 && vitorias <= 100) {
-        nivel = 'Lendário';
-    } else if (vitorias >= 101) {
-        nivel = 'Imortal';
-    } else {
-        nivel = 'Desconhecido';
+class perfilHeroi{
+    constructor(nome, idade,tipo, ataque){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        this.ataque = ataque
     }
 
-    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+    escrever(){
+        console.log(`O ${this.nome} é um ${this.tipo} com ${this.idade} anos de idade e atacou usando ${this.ataque}`)
+    }
 }
+let guerreiro = new perfilHeroi("Ragnar", 35, "guerreiro","espada")
 
-let resultado1 = calcularNivel(25, 5);
-let resultado2 = calcularNivel(50, 10);
-let resultado3 = calcularNivel(90, 45);
-
-console.log(resultado1);
-console.log(resultado2);
-console.log(resultado3);
+guerreiro.escrever()
